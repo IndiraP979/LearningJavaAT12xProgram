677dbd500f798fbf01485179d5bc7274c756f4dd
IndiraP979 <indirap.pd@gmail.com>
2025-05-14 12:19:39 +0530

    ex_01_Java_Basics

---
+++ src/ex_03_Literals/Lab036_Task_11_May.java
@@ -0,0 +1,20 @@
+package ex_03_Literals;
+
+public class Lab036_Task_11_May
+{
+    public static void main(String[] args) {
+        int num=5;
+        System.out.println("------------");
+        System.out.println("5x1="+num);
+        System.out.println("5x2="+num*2);
+        System.out.println("5x3="+num*3);
+        System.out.println("5x4="+num*4);
+        System.out.println("5x5="+num*5);
+        System.out.println("5x6="+num*6);
+        System.out.println("5x7="+num*7);
+        System.out.println("5x8="+num*8);
+        System.out.println("5x9="+num*9);
+        System.out.println("5x10="+num*10);
+        System.out.println("-------------");
+    }
+}
