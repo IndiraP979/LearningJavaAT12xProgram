c1952c2565e6f3871b19724061ca864103c30a83
IndiraP979 <indirap.pd@gmail.com>
2025-05-14 11:45:09 +0530

    ex_01_Java_Basics

---
+++ src/ex_02_Java_Basics_Part2/Lab020_Interview_Qna.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab020_Interview_Qna
+{
+    public static void main(String[] args)
+    {
+        //int enum = 10; keyword used
+        int Enum = 10;
+        int a_b = 89;
+//      int pramod dutta  = 89;space used
+        int pramod$dutta = 90;
+        int Lab020_Interview_QnA2 = 123;
+        System.out.println(Enum);
+        System.out.println(a_b);
+        System.out.println(pramod$dutta);
+        System.out.println(Lab020_Interview_QnA2);
+
+    }
+}
