57c2861f0f7d1ffd8be0a9ce09c8aac5e68fef91
IndiraP979 <indirap.pd@gmail.com>
2025-05-17 12:19:21 +0530

    ex_01_Java_Basics

--- src/ex_06_Ternary_Operator/Lab067_Task2_17May.java
+++ src/ex_06_Ternary_Operator/Lab067_Task2_17May.java
@@ -5,7 +5,7 @@ public class Lab067_Task2_17May
     public static void main(String[] args) {
         int a=3;int b=4;int c=1;
 
-        String result= a>b?(a>c?"a is greater": "c is greater"):(b>a?(b>c?"b is gretaer": "c is greater"): "c is greter");
+        String result= a>b?(a>c?"a is greater": "c is greater"):(b>a?(b>c?"b is greater": "c is greater"): "c is greater");
         System.out.println( result);
     }
 }
