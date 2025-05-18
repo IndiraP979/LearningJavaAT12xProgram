28162923265e1f46abe590da254d48a4044b8715
IndiraP979 <indirap.pd@gmail.com>
2025-05-18 12:04:59 +0530

    ex_01_Java_Basics

---
+++ src/ex_07_Increment_Decrement_Op/Lab075076_Exp1.java
@@ -0,0 +1,14 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab075076_Exp1
+{
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a++ + ++a);
+        System.out.println(a);
+
+        int b = 10;
+        System.out.println(++b + ++b);
+        System.out.println(b);
+    }
+}
