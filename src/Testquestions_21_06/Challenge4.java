30d898f021d30add602d429bcf950590bf3f701c
IndiraP979 <indirap.pd@gmail.com>
2025-06-21 11:58:30 +0530

    Challenge4

---
+++ src/Testquestions_21_06/Challenge4.java
@@ -0,0 +1,19 @@
+package Testquestions_21_06;
+
+import java.util.Scanner;
+
+//a == b: false a > b: true a < b: false (a > b) && (a > 0): true (a < b) || (a > 0): true !(a > b): false
+public class Challenge4
+{
+    public static void main(String[] args)
+    {
+
+        int a = 5;
+
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(a--);
+
+    }
+}
