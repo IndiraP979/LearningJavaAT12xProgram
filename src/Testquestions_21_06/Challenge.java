fd70c553ba26a48ec69bc03be5c4356a5f6e15b6
IndiraP979 <indirap.pd@gmail.com>
2025-06-21 11:40:48 +0530

    Challenge1

---
+++ src/Testquestions_21_06/Challenge.java
@@ -0,0 +1,21 @@
+package Testquestions_21_06;
+
+public class Challenge
+{
+    public static void main(String[] Args) {
+//Integer: 25 Double: 3.14 Boolean: true Character: A
+
+        int Integer = 25;
+        double Double = 3.14;
+        boolean Boolean = true;
+        char Character = 'A';
+        System.out.println("Integer:"+Integer);
+        System.out.println("Double:"+Double);
+        System.out.println("Boolean:"+Boolean);
+        System.out.println("Character:"+Character);
+    }
+
+
+
+
+}
