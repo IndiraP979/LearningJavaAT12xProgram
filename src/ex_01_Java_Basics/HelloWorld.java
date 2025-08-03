c31da52a36ec6915acae119c644e366e4602d53d
IndiraP979 <indirap.pd@gmail.com>
2025-08-03 09:36:39 +0530

    test new

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -1,7 +1,9 @@
 package ex_01_Java_Basics;
+// this is for testing purpose
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello world");
         System.out.println("Hello world");
     }
 }
